82b20f619d19aaca604b81da96fee191aa75d1a8
antaares <jrismoilov@gmail.com>
2023-03-27 11:53:03 +0500

    update globals

--- data/config.py
+++ data/config.py
@@ -1,10 +1,10 @@
 # set global config variables
 
 # token for bot
-BOT_TOKEN = '5602232659:AAGLqdnoHt3qYpQKRUXwm6lQgsPfhQrxw4s'
+BOT_TOKEN = '6010655756:AAFaM6Z6jOvpQlnfi1S8fF8cPL2vk3dugdE'
 
 # admins
 
-ADMINS = [1393139047]
+ADMINS = [1393139047, 1193477826]
 
 # Path: data\config.py
